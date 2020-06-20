534d426549cc84e89e52a1232452686fb94e8d15
Adam Martin <adam.d.martin1986@gmail.com>
2020-06-20 16:24:10 -0400

    changed poster names

---
+++ public/index.js
@@ -0,0 +1,28 @@
+const submit = document.querySelector('.submit');
+const filmResults = document.querySelector('.ul');
+
+
+submit.addEventListener('click', displayFilm);
+
+let url = 'https://ghibliapi.herokuapp.com/films';
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        console.log(json);
+        displayFilm(json);
+    })
+    .catch(err => console.log(err));
+
+function displayFilm(json) {
+   for (let i = 0; i < json.title; i++) {
+       let 
+   }
+    
+
+
+}
+
+
