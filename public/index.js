972aba23d8746090784ce6065137c25d38f67f3d
Adam Martin <adam.d.martin1986@gmail.com>
2020-06-22 20:06:29 -0400

    partically fixed function

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 const submit = document.querySelector('.submit');
-const filmResults = document.querySelector('.ul');
+const filmResults = document.querySelector('ul');
 
 
 submit.addEventListener('click', displayFilm);
@@ -7,24 +7,28 @@ submit.addEventListener('click', displayFilm);
 let url = 'https://ghibliapi.herokuapp.com/films';
 
 fetch(url)
-    .then(function (response) {
+    .then(function(response) {
         return response.json();
     })
-    .then(function (json) {
+    .then(function(json) {
         console.log(json);
+        displayFilm(json)
     })
     .catch(err => console.log(err));
 
+
+
 function displayFilm(json) {
-   for (let i = 0; i < json.results.title; i++) {
-       let title = document.createElement('li');
-       title.src = json.results.title
-
-       document.body.appendChild(list);
-   }
-    
 
 
-}
+    json.forEach(element => {
+        console.log(element.title)
+        console.log(element.description)
+        
 
 
+    }
+
+
+
+)};
